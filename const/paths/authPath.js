4ccf4b9dcd2670cde0441acec919366cd234750d
Bogdan Protasenko <b.protasenko@gmail.com>
2024-02-02 22:11:02 +0100

    path change

--- const/paths/authPath.js
+++ const/paths/authPath.js
@@ -1,12 +1,12 @@
 const authPath = {
-  BASE: '/auth',
+  ROOT: '/api/auth',
+  BASE: '/',
   SIGN_IN: '/signin',
   SIGN_UP: '/signup',
   VERIFY: '/verify',
   VERIFY_CODE: '/verify/:verificationCode',
   CURRENT: '/current',
   SIGNOUT: '/signout',
-  ROOT: '/',
   AVATARS: '/avatars',
 };
 
