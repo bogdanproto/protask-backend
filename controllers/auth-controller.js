c8812aacebc70a9b1c2081be60d7f9373935d97c
Valentina <valentinasergssvna@gmail.com>
2024-02-03 14:51:03 +0200

    added signup.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -4,42 +4,12 @@ import "dotenv/config";
 
 import User from "../models/User.js";
 
-import { userStatus, successStatus } from "../const/index.js";
+import { userStatus } from "../const/index.js";
 import httpError from "../helpers/errorHandlers/httpError.js";
 import { ctrlWrapper } from "../decorators/index.js";
+import {signup} from "./auth/index.js";
 
 const {JWT_SECRET} = process.env;
-
-const signup = async (req, res) => {
-    const {email, password} = req.body;
-    const user = await User.findOne({email});
-    if (user) {
-        throw httpError(userStatus.USER_CONFLICT);
-    }
-
-    const hashPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ ...req.body, password: hashPassword });
-
-    const { _id: id } = newUser;
-    const payload = {
-        id
-    };
-    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
-    await User.findByIdAndUpdate(id, {token});
-
-    res.status(successStatus.CREATED.status).json({
-        ...successStatus.CREATED,
-        data: {
-            user: {
-                userName: newUser.userName,
-                email: newUser.email,
-                theme: newUser.theme,
-                avatarCloudURL: newUser.avatarCloudURL,
-            },
-            token,
-        }
-    })
-}
 
 const signin = async(req, res)=> {
     const {email, password} = req.body;
