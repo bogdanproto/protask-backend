7bd434e452802cc99a30beb647a1131a80c767c2
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2024-02-07 18:43:27 +0200

    update controllers of COLUMNS

--- controllers/columns/deleteColumn.js
+++ controllers/columns/deleteColumn.js
@@ -9,15 +9,17 @@ export const deleteColumn = async (req, res) => {
   const { _id: owner } = req.user;
 
   const column = await Column.findOne({ _id, owner });
-  const board = await Board.findOne({ _id: column.board, owner });
-  const result = await column.deleteOne();
 
-  if (!result) {
+  if (!column) {
     throw HttpError(errorStatus.NOT_FOUND_COLUMN);
   }
 
+  const board = await Board.findOne({ _id: column.board, owner });
+  const { title } = column;
+
+  await column.deleteOne();
   await board.updateOne({ $pull: { columns: column._id } });
   await Card.deleteMany({ column: _id, owner });
 
-  res.json({ ...successStatus.DELETED_COLUMN, data: { title: column.title } });
+  res.json({ ...successStatus.DELETED_COLUMN, data: { title } });
 };
