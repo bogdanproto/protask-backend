aba56cc6630f740a9c2282740adb31e91279f580
Valentina <valentinasergssvna@gmail.com>
2024-02-11 21:12:20 +0200

    Added Update User Profile to swagger

--- schemas/User/joi/userSignupSchema.js
+++ schemas/User/joi/userSignupSchema.js
@@ -3,7 +3,7 @@ import { commonUserValidator } from "../../../const/index.js";
 
 const userSignupSchema = Joi.object({
     userName: Joi.string().min(2).required(),
-    password: Joi.string().min(6).required(),
+    password: Joi.string().min(8).required(),
     email: Joi.string().pattern(commonUserValidator.EMAIL_REGEXP).required(),
 });
 
