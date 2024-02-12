2cabdd684547c2f7ea3defbff3444cba794812ff
Valentina <valentinasergssvna@gmail.com>
2024-02-12 21:40:18 +0200

    Added "Need Help" API to Swagger

--- controllers/need-help/needHelp.js
+++ controllers/need-help/needHelp.js
@@ -1,20 +1,17 @@
-import { sendEmail, HttpError }  from '../../helpers/index.js';
+import { sendEmail }  from '../../helpers/index.js';
 
-const { SUPPORT_SERVICE} = process.env;
+const { SUPPORT_SERVICE } = process.env;
 
 const needHelp = async (req, res) => {
     const { email, comment } = req.body;
-    if (!email || !comment ) {
-        throw HttpError(404, "Please provide email and text");
-      }
 
-      const data = {
-        from: email,
-        to:  SUPPORT_SERVICE,
-        subject: 'Help Request',
-        html: comment,
-      };
-      await sendEmail(data);
+    const data = {
+      from: email,
+      to:  SUPPORT_SERVICE,
+      subject: 'Help Request',
+      html: comment,
+    };
+    await sendEmail(data);
 
   res.json({
     message: "Your comment has been sent",
